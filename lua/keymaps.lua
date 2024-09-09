c7e849744be409cad9b9adef5068377af8d7f7a8
Adam Rich <rich.178@osu.edu>
2024-09-09 10:12:16 -0400

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,9 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- custom return to file system
+vim.keymap.set('n', '<leader>pv', vim.cmd.EX)
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
